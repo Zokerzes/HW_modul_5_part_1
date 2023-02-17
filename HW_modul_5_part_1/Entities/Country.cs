8a09e036e78e034273c34e1b18450de76d7c6a7d
Zokerzes <Bliz_mw86@itstepru.onmicrosoft.com>
2023-02-17 19:35:08 +0300

    ++

--- HW_modul_5_part_1/Entities/Country.cs
+++ HW_modul_5_part_1/Entities/Country.cs
@@ -1,4 +1,5 @@
 ﻿using AdoNetWinformsApp.Entities.Constants;
+using HW_modul_5_part_1.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,6 @@ namespace AdoNetWinformsApp.Entities
         public string Name { get; set; } = string.Empty;
         public decimal Area { get; set; }
         public PartOfWord PartOfWord { get; set; }
+        public virtual ICollection<City> Cities { get; set; }
     }
 }
--- HW_modul_5_part_1/EntityMappings/CityEntityMapping.cs
+++ HW_modul_5_part_1/EntityMappings/CityEntityMapping.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AdoNetWinformsApp.Entities;
+using AdoNetWinformsApp.EntityMappings;
+using HW_modul_5_part_1.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +11,16 @@ using System.Threading.Tasks;
 
 namespace HW_modul_5_part_1.EntityMappings
 {
-    public class CityEntityMapping
+    public class CityEntityMapping : IEntityTypeConfiguration<City>
     {
-
+        public void Configure(EntityTypeBuilder<City> builder)
+        {
+            builder
+               .Property(x => x.Name)
+               .HasMaxLength(DataBaseDefault.StringValueMaxLenght);
+            builder.HasOne(x => x.Country)
+               .WithMany(x=>x.Cities)
+               .HasForeignKey(x => x.CountryId);
+        }
     }
 }
