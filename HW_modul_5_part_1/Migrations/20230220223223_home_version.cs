f532ea4efdcbaa299f1cee5294f9beed29a4a61e
Zokerzes <yozhik13@gmail.com>
2023-02-21 01:36:39 +0300

    попытка сохранить данные

---
+++ HW_modul_5_part_1/Migrations/20230220223223_home_version.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HW_modul_5_part_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class home_version : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
