d9899d8d2e04dbcaa029f22f4c5fd9bfde16c0db
Zokerzes <Bliz_mw86@itstepru.onmicrosoft.com>
2023-02-27 21:16:37 +0300

    ++ add migration

---
+++ HW_modul_5_part_1/Migrations/20230227181606_new1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HW_modul_5_part_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class new1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
